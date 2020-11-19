6c9b7deac4609c99bbe1b714cc6cb64586edfb55
Vivek3927 <vivek.vas27@gmail.com>
2020-11-19 15:08:39 +0530

    Changes of Api

--- src/Components/Views/Header.js
+++ src/Components/Views/Header.js
@@ -1,26 +1,18 @@
 import React from "react";
+import AutoComplete from "../searchData/AutoComplete";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="container-fluid p-3 Nav">
       <div className="row">
-        <div className="col-md-4"></div>
         <div className="col-md-4">
           <h3 className="TextEdit">Indian Premier League</h3>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-8">
           <div className="d-flex justify-content-end">
             <form className="form-inline" />
-            <input
-              className="form-control w-50"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-info " type="submit">
-              Search
-            </button>
+            <AutoComplete />
           </div>
         </div>
       </div>
--- src/Components/searchData/Search.js
+++ src/Components/searchData/Search.js
@@ -2,64 +2,68 @@ import React from "react";
 import "./Search.css";
 import Table from "react-bootstrap/Table";
 
-const Search = () => {
+const Search = (props) => {
+  console.log(props);
   return (
-    <div className="container bg-light">
-      <div className=""></div>
-      <div className="container bg-light">
-        <div className="row">
-          <div className="col-md-2">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/2/25/Sachin_Tendulkar_at_MRF_Promotion_Event.jpg"
-              alt="Sachin"
-              className="Image"
-            />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 mx-md-auto">
+          <div className="container bg-light">
+            <div className="row">
+              <div className="col-md-6">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/2/25/Sachin_Tendulkar_at_MRF_Promotion_Event.jpg"
+                  alt="Sachin"
+                  className="Image"
+                />
+              </div>
+              <div className="col-md-9  align-self-end NameInfo">
+                <h1>Sachin Tendulkar</h1>
+                <p className="India">India</p>
+              </div>
+            </div>
           </div>
-          <div className="col-md-9 align-self-end NameInfo">
-            <h1>Sachin Tendulkar</h1>
-            <p className="India">India</p>
-          </div>
-        </div>
-      </div>
-      <div className="container mt-3 ">
-        <div className="row">
-          <div className="col-md-5 bg-light p-2 ">
-            <h5>Personal Information</h5>
-            <Table borderless>
-              <tbody>
-                <tr>
-                  <td className="TextBold">Born</td>
-                  <td>Apr 24, 1973 (47 years)</td>
-                </tr>
-                <tr>
-                  <td className="TextBold">Birth Place</td>
-                  <td>Bombay (now Mumbai), Maharashtra</td>
-                </tr>
-                <tr>
-                  <td className="TextBold">Height</td>
-                  <td>5 ft 5 in</td>
-                </tr>
-                <tr>
-                  <td className="TextBold">Role</td>
-                  <td>Batsman</td>
-                </tr>
-                <tr>
-                  <td className="TextBold">Batting Style</td>
-                  <td>Right Handed Bat</td>
-                </tr>
-                <tr>
-                  <td className="TextBold">Bowling Style</td>
-                  <td>Right-arm legbreak</td>
-                </tr>
-              </tbody>
-            </Table>
-          </div>
-          <div className="col-md-7">
-            <h3 className="ml-5 mt-3">Sachiiiiiiin, Sachin!</h3>
-            <p className="col-md mt-5">
-              You’ve heard tde chant. You may not have heard of tde sport, but you’ve heard the
-              chant. The chant - one that a billion people once l...
-            </p>
+          <div className="container mt-3 ">
+            <div className="row">
+              <div className="col-md-5 bg-light p-2 ">
+                <h5>Personal Information</h5>
+                <Table borderless>
+                  <tbody>
+                    <tr>
+                      <td className="TextBold">Born</td>
+                      <td>Apr 24, 1973 (47 years)</td>
+                    </tr>
+                    <tr>
+                      <td className="TextBold">Birth Place</td>
+                      <td>Bombay (now Mumbai), Maharashtra</td>
+                    </tr>
+                    <tr>
+                      <td className="TextBold">Height</td>
+                      <td>5 ft 5 in</td>
+                    </tr>
+                    <tr>
+                      <td className="TextBold">Role</td>
+                      <td>Batsman</td>
+                    </tr>
+                    <tr>
+                      <td className="TextBold">Batting Style</td>
+                      <td>Right Handed Bat</td>
+                    </tr>
+                    <tr>
+                      <td className="TextBold">Bowling Style</td>
+                      <td>Right-arm legbreak</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+              <div className="col-md-7">
+                <h3 className="ml-5 mt-3">Sachiiiiiiin, Sachin!</h3>
+                <p className="col-md mt-5">
+                  You’ve heard tde chant. You may not have heard of tde sport, but you’ve heard the
+                  chant. The chant - one that a billion people once l...
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
